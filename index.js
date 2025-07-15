fdf30a306801fd3c267f1419f2727ba4472e04ee
molikpunj <moulickpunjdu@gmail.com>
2025-07-16 04:14:46 +0530

    Added 1 player page

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+document.getElementById("1player").onclick = function() {
+    window.location.href="1player.html";
+}
+
 document.getElementById("2player").onclick = function() {
     window.location.href="2player.html";
 }
