416e26465554aa11ba8cad8e42a0f21acd9f3835
molikpunj <moulickpunjdu@gmail.com>
2025-07-18 21:40:58 +0530

    Made the backend so that on clicking, it will alternatively assign x or o to a box

---
+++ 2player.js
@@ -0,0 +1,24 @@
+const boxes = document.querySelectorAll(".divbox");
+let xtrue = true;
+
+boxes.forEach(function(b) {
+    b.onclick = function() {
+        if (b.textContent == ''){
+            if (xtrue) {
+                xshow(b);
+                xtrue = false;
+            } else {
+                oshow(b);
+                xtrue = true;
+            }
+        }
+    };
+});
+
+function xshow(box) {
+    box.textContent = "X";
+}
+
+function oshow(box) {
+    box.textContent = "O";
+}
