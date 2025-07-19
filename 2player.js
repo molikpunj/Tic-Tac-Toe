ad45c45b91a7a8ca38fa746e2b4ce420e6f051d5
molikpunj <moulickpunjdu@gmail.com>
2025-07-20 03:04:32 +0530

    Wrote the winning combinations

--- 2player.js
+++ 2player.js
@@ -1,6 +1,16 @@
 const boxes = document.querySelectorAll(".divbox");
 let xtrue = true;
 let board = ["", "", "", "", "", "", "", "", ""];
+const winningcombo = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 boxes.forEach(function(b) {
     b.onclick = function() {
@@ -15,6 +25,10 @@ boxes.forEach(function(b) {
         }
     };
 });
+
+function checkwinner() {
+    
+}
 
 function xshow(box) {
     box.textContent = "X";
