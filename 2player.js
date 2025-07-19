c0a95f50159198e1e157b54655a3eb076cb6810e
molikpunj <moulickpunjdu@gmail.com>
2025-07-20 02:51:25 +0530

    Made it so that an array registers which box contains which character

--- 2player.js
+++ 2player.js
@@ -1,5 +1,6 @@
 const boxes = document.querySelectorAll(".divbox");
 let xtrue = true;
+let board = ["", "", "", "", "", "", "", "", ""];
 
 boxes.forEach(function(b) {
     b.onclick = function() {
@@ -17,14 +18,24 @@ boxes.forEach(function(b) {
 
 function xshow(box) {
     box.textContent = "X";
+    let i = parseInt(box.id);
+    board[i] = box.textContent;
 }
 
 function oshow(box) {
     box.textContent = "O";
+    let i = parseInt(box.id);
+    board[i] = box.textContent;
 }
 
 document.getElementById("reset").onclick = function() {
     boxes.forEach(function(b) {
         b.textContent = "";
     });
+}
+
+document.getElementById("resetscore").onclick = function() {
+for(let j=0; j<=8; j++){
+    console.log(board[j]);
+}
 }
