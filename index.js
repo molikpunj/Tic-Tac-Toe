1bdc8d980e1c28ac95c902d838841afccee3c2d1
molikpunj <moulickpunjdu@gmail.com>
2025-07-27 03:32:29 +0530

    Added project documentation page

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ document.getElementById("1player").onclick = function() {
 document.getElementById("2player").onclick = function() {
     window.location.href="2player.html";
 }
+
+document.getElementById("how").onclick = function() {
+    window.location.href = "project.html";
+}
